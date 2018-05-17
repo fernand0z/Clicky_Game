886897159b4bd4fc4bbd67b044abad11a6054164
Fernando <fernandoz@outlook.com>
2018-05-16 20:53:02 -0700

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,62 +1,64 @@
-[
-  {
-    breed: "bulldog",
-    image: "https://dog.ceo/api/img/bulldog-boston/n02096585_1737.jpg"
-  },
-  {
-    breed: "beagle",
-    image: "https://dog.ceo/api/img/beagle/n02088364_13050.jpg"
-  },
-  {
-    breed: "husky",
-    image: "https://dog.ceo/api/img/husky/n02110185_14523.jpg"
-  },
-  {
-    breed: "maltese",
-    image: "https://dog.ceo/api/img/maltese/n02085936_10197.jpg"
-  },
-  {
-    breed: "pomeranian",
-    image: "https://dog.ceo/api/img/pomeranian/n02112018_13636.jpg"
-  },
-  {
-    breed: "poodle",
-    image: "https://dog.ceo/api/img/poodle-miniature/n02113712_1088.jpg"
-  },
-  {
-    breed: "pyrenees",
-    image: "https://dog.ceo/api/img/pyrenees/n02111500_395.jpg"
-  },
-  {
-    breed: "chihuahua",
-    image: "https://dog.ceo/api/img/chihuahua/n02085620_3409.jpg"
-  },
-  {
-    breed: "boxer",
-    image: "https://dog.ceo/api/img/boxer/n02108089_11687.jpg"
-  },
-  {
-    breed: "chow",
-    image: "https://dog.ceo/api/img/chow/n02112137_12792.jpg"
-  },
-  {
-    breed: "corgi",
-    image: "https://dog.ceo/api/img/corgi-cardigan/n02113186_1030.jpg"
-  },
-  {
-    breed: "mastiff",
-    image: "https://dog.ceo/api/img/mastiff-bull/n02108422_2052.jpg"
-  },
-  {
-    breed: "sheepdog",
-    image: "https://dog.ceo/api/img/sheepdog-english/n02105641_12614.jpg"
-  },
-  {
-    breed: "terrier",
-    image: "https://dog.ceo/api/img/terrier-american/n02093428_10164.jpg"
-  },
-  {
-    breed: "shihtzu",
-    image: "https://dog.ceo/api/img/shihtzu/n02086240_11766.jpg"
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import cards from "./cards.json";
+import "./App.css";
+
+class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards,
+    score: 0,
+    highscore: 0
+  };
+
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
   }
-];
+
+  clickCount = id => {
+    this.state.cards.find((o, i) => {
+      if (o.id === id) {
+        if(cards[i].count === 0){
+          cards[i].count = cards[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+  // Map over this.state.cards and render a cardCard component for each card object
+  render() {
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        {this.state.cards.map(card => (
+          <Card
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,13 @@
 import React from "react";
-import "./Title.css";
+import "./Header.css";
 
-const Title = props => <h1 className="title">{props.children}</h1>;
+const Header = props => (
+  <div className="header">
+    <div className="title">{props.children}</div>
+    <div className="scores">
+      Score: {props.score} Highscore: {props.highscore}
+    </div>
+  </div>
+);
 
-export default Title;
+export default Header;
