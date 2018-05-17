dd295df740435ed0c8ed2337633d58c662bf36db
Fernando <fernandoz@outlook.com>
2018-05-16 18:18:47 -0700

    Starting file structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FormOne, FormTwo, FormThree } from "./components/Form";
+
+const App = () => (
+  <div>
+    <FormOne />
+    <FormTwo />
+    <FormThree />
+  </div>
+);
+
+export default App;
